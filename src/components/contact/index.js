6a1b8c0949f32ae281e8569090eacb2a9fb746cf
gertoad <theyayo47@gmail.com>
2022-07-13 16:59:56 -0500

    finishing structure

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,13 +1,34 @@
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
+
+const emailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+const github = <FontAwesomeIcon icon={faGithub} />;
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} />;
+const email = "theyayo47@gmail.com";
+
+const links = [
+  {socilMedia: "linkedin", icon: linkedIn, url: "https://www.linkedin.com/in/gerardo-palmieri-mondrag%C3%B3n-486485203/"},
+  {socilMedia: "github", icon: github, url: "https://github.com/GerToad/Gerardo-Palmieri-Mondragon"},
+];
 
   return(
     <div className="contact">
         <section className="container">
           <h2>Contact</h2>
           <p>Let's work together</p>
-          <span>theyayo47@gmail.com</span>
+          <button onClick={() => {navigator.clipboard.writeText(email)}} className="copy">{emailIcon} {email} </button>
+
+          <ul className="links">
+            {links.map(link => (
+              <li key={link.socialMedia}>
+                <a href={link.url} className={link.socilMedia}>{link.icon}</a>
+              </li>
+            ))}
+          </ul>
         </section>
     </div>
   )
